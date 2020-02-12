38e06fdebc1aba75b4486b0cdadf812b11d79b35
ytnobody <ytnobody@gmail.com>
2020-02-12 18:41:01 +0900

    add documentation

--- README.md
+++ README.md
@@ -1,14 +1,51 @@
 # NAME
 
-Test::MockServer::Postgresql - It's new $module
+Test::MockServer::Postgresql - A Postgresql mock server for testing perl programs
 
 # SYNOPSIS
 
+    use Test::More;
     use Test::MockServer::Postgresql;
+    
+    # raunch a container from postgres:12-alpine image
+    my $server = Test::MockServer::Postgresql->new(version => 12, distro => "alpine");
+    $server->run();
+    
+    # puke initialization data into postgresql on a container
+    $server->fixture("/path/to/fixture.sql");
+    
+    # get a Database Handler(a DBI::db object) from mock server object
+    my $dbh = $server->dbh();
+    my $sth = $dbh->prepare("SELECT * FROM Users WHERE id=?");
+    $sth->execute(1);
+    
+    # put your own test code below
+    my $row $sth->fetchrow_hashref();
+    is $row->{name}, "ytnobody";
+    
+    done_testing;
 
 # DESCRIPTION
 
-Test::MockServer::Postgresql is ...
+Test::MockServer::Postgresql run the postgres container on the Docker, for testing your perl programs.
+
+**\*\*NOTE\*\*** Maybe this module doesn't work on the Windows, because this module uses some backticks for use the Docker.
+
+# OPTIONS FOR CONSTRACTOR
+
+- distro (int)
+
+    A distribution name of container OS. You may specify "debian" or "alpine". Default is "debian".
+
+- version (str)
+
+    A version number of the PostgreSQL. Default is 12. 
+
+# REQUIREMENT
+
+- Docker
+
+    This module uses the Docker as ephemeral environment.
 
 # LICENSE
 
@@ -20,3 +57,7 @@ it under the same terms as Perl itself.
 # AUTHOR
 
 Satoshi Azuma <ytnobody@gmail.com>
+
+# SEE ALSO
+
+[https://hub.docker.com/\_/postgres](https://hub.docker.com/_/postgres)
--- lib/Test/MockServer/Postgresql.pm
+++ lib/Test/MockServer/Postgresql.pm
@@ -53,7 +53,7 @@ sub run {
 
 sub fixture {
     my ($self, $filepath) = @_;
-    my $dbh = $self->dbh(); ## DB接続ができる様になるまで待つ役割
+    my $dbh = $self->dbh(); ## waiting for DB connection
     my $ctname = $self->container_name();
     my $dbname = $self->{dbname};
     my $user = $self->{user};
@@ -95,15 +95,60 @@ __END__
 
 =head1 NAME
 
-Test::MockServer::Postgresql - It's new $module
+Test::MockServer::Postgresql - A Postgresql mock server for testing perl programs
 
 =head1 SYNOPSIS
 
+    use Test::More;
     use Test::MockServer::Postgresql;
+    
+    # raunch a container from postgres:12-alpine image
+    my $server = Test::MockServer::Postgresql->new(version => 12, distro => "alpine");
+    $server->run();
+    
+    # puke initialization data into postgresql on a container
+    $server->fixture("/path/to/fixture.sql");
+    
+    # get a Database Handler(a DBI::db object) from mock server object
+    my $dbh = $server->dbh();
+    my $sth = $dbh->prepare("SELECT * FROM Users WHERE id=?");
+    $sth->execute(1);
+    
+    # put your own test code below
+    my $row $sth->fetchrow_hashref();
+    is $row->{name}, "ytnobody";
+    
+    done_testing;
 
 =head1 DESCRIPTION
 
-Test::MockServer::Postgresql is ...
+Test::MockServer::Postgresql run the postgres container on the Docker, for testing your perl programs.
+
+B<**NOTE**> Maybe this module doesn't work on the Windows, because this module uses some backticks for use the Docker.
+
+=head1 OPTIONS FOR CONSTRACTOR
+
+=over 2
+
+=item distro (int)
+
+A distribution name of container OS. You may specify "debian" or "alpine". Default is "debian".
+
+=item version (str)
+
+A version number of the PostgreSQL. Default is 12. 
+
+=back
+
+=head1 REQUIREMENT
+
+=over 2
+
+=item Docker
+
+This module uses the Docker as ephemeral environment.
+
+=back
 
 =head1 LICENSE
 
@@ -115,6 +160,10 @@ it under the same terms as Perl itself.
 =head1 AUTHOR
 
 Satoshi Azuma E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+L<https://hub.docker.com/_/postgres>
 
 =cut
 
